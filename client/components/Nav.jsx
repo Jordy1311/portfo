6f551ea70a996d13bbdcdd2190fc93440840ff11
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-20 16:06:51 +1200

    pad out Nav with initial content

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>
-      
+    <div className='Nav'>
+      <img src="/images/logo-color.png" alt="Jordan's cool logo is <j.cooper> with colored in o's'" className="logo-main" />
+      <div className="nav_linkgroup">
+        <a href="#about" className="nav_link">about</a>
+        <a href="#projects" className="nav_link">projects</a>
+        <a href="#contact" className="nav_link">contact</a>
+      </div>
     </div>
   )
 }
