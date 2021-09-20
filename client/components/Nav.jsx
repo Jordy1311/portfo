64bd7688d53c0ad2d40e669726422f87cde0889b
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-20 17:49:20 +1200

    changed double quotes to mostly single

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className='Nav'>
-      <img src="/images/logo-color.png" alt="Jordan's cool logo is <j.cooper> with colored in o's'" className="nav--logo" />
-      <div className="nav--linkgroup">
-        <a href="#about" className="nav--link">about</a>
-        <a href="#projects" className="nav--link">projects</a>
-        <a href="#contact" className="nav--link">contact</a>
+      <img src='/images/logo-color.png' alt="Jordan's cool logo is <j.cooper> with colored in o's" className='nav--logo' />
+      <div className='nav--linkgroup'>
+        <a href='#about' className='nav--link'>about</a>
+        <a href='#projects' className='nav--link'>projects</a>
+        <a href='#contact' className='nav--link'>contact</a>
       </div>
     </div>
   )
