b5b13c2cdbc76d07a7adf42e4bf332baacb18928
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-20 13:23:15 +1200

    template components

---
+++ client/components/LandingTypewriter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingTypewriter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LandingTypewriter
---
+++ client/components/ProjectsLang.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectsLang = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProjectsLang
