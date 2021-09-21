3a695590f8db3326f918c5507bb9311d299725d1
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-21 15:44:48 +1200

    remove data from component and import from data.js

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -3,49 +3,20 @@ import React from 'react'
 import Project from './Project'
 import ProjectsLang from './ProjectsLang'
 
-const Projects = () => {
-  const projects = [
-    {
-      title: "project 1",
-      img: "https://source.unsplash.com/random/800x600",
-      imgDescription: "This is a photo of my project"
-    },
-    {
-      title: "project 2",
-      img: "https://source.unsplash.com/random/800x600",
-      imgDescription: "This is a photo of my project"
-    },
-    {
-      title: "project 3",
-      img: "https://source.unsplash.com/random/800x600",
-      imgDescription: "This is a photo of my project"
-    }
-  ]
+import data from '../data.js'
 
-  const langs = [
-    {
-      name: "CSS",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png"
-    },
-    {
-      name: "Something",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png"
-    },
-    {
-      name: "SOMETHING ELSE",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png"
-    }
-  ]
+const Projects = () => {
+  const { projectsData, langsData } = data
 
   return (
     <div className='Projects'>
       <h1>projects</h1>
       <div className="projects--projects-group">
-        {projects.map(project => <Project key={project.title} project={project} />)}
+        {projectsData.map(project => <Project key={project.title} project={project} />)}
       </div>
       <h1>tools & langs</h1>
       <div className="projects--langs-group">
-        {langs.map(tool => <ProjectsLang key={tool.name} tool={tool} />)}
+        {langsData.map(tool => <ProjectsLang key={tool.name} tool={tool} />)}
       </div>
     </div>
   )
