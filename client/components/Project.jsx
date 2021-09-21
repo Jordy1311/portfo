95282d66bdf84a2c44fa3856a08da76a337d24af
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-21 15:35:00 +1200

    add links to projects and langs

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Project = ({ project }) => {
   return (
-    <div className='Project'>
+    <a href={project.link} target='__blank' className='Project'>
       <img src={project.img} alt={project.imgDescription} className='project--image' />
       <h4>{project.title}</h4>
-    </div>
+    </a>
   )
 }
 
--- client/components/ProjectsLang.jsx
+++ client/components/ProjectsLang.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProjectsLang = ({ tool }) => {
   return (
-    <div className='ProjectLang'>
+    <a href={tool.link} target='__blank' className='ProjectLang'>
       <img src={tool.img} alt={tool.name} className='projectlang--image' />
       <h4>{tool.name}</h4>
-    </div>
+    </a>
   )
 }
 
