ded234b0b6247f272624882834f85e61338a3a40
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-21 13:48:44 +1200

    bringing in Projects and initial content

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Landing from './Landing'
 import AboutMe from './AboutMe'
+import Projects from './Projects'
 
 const App = () => {
   return (
     <>
       <Landing />
       <AboutMe />
+      <Projects />
     </>
   )
 }
--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 
+import Project from './Project'
+import ProjectsLang from './ProjectsLang'
+
 const Projects = () => {
+  const projects = [
+    {
+      title: "project 1",
+      img: "https://source.unsplash.com/random/800x600",
+      imgDescription: "This is a photo of my project"
+    },
+    {
+      title: "project 2",
+      img: "https://source.unsplash.com/random/800x600",
+      imgDescription: "This is a photo of my project"
+    },
+    {
+      title: "project 3",
+      img: "https://source.unsplash.com/random/800x600",
+      imgDescription: "This is a photo of my project"
+    }
+  ]
+
+  const langs = [
+    {
+      name: "CSS",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png"
+    },
+    {
+      name: "Something",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png"
+    },
+    {
+      name: "SOMETHING ELSE",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png"
+    }
+  ]
+
   return (
-    <div>
-      
+    <div className='Projects'>
+      <h1>projects</h1>
+      <div className="projects--projects-group">
+        {projects.map(project => <Project key={project.title} project={project} />)}
+      </div>
+      <h1>tools & langs</h1>
+      <div className="projects--langs-group">
+        {langs.map(tool => <ProjectsLang key={tool.name} tool={tool} />)}
+      </div>
     </div>
   )
 }
