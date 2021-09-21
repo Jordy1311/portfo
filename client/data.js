e041f6d193595883936a5c373fb63cb378e74750
Jordan Cooper <jordancooper1195@gmail.com>
2021-09-21 15:35:51 +1200

    create data.js to hold my data

---
+++ client/data.js
@@ -0,0 +1,59 @@
+export default {
+  projectsData: [
+    {
+      title: "commitCanvas",
+      img: "/images/projects/commitCanvas.png",
+      imgDescription: "An example of the cube art that get's output from commitCanvas",
+      link: "https://github.com/Jordy1311/commitCanvas"
+    },
+    {
+      title: "Calculator",
+      img: "/images/projects/calculator.png",
+      imgDescription: "A quaint retro calculator",
+      link: "https://jordy1311.github.io/calculator/"
+    },
+    {
+      title: "Minesweeper",
+      img: "/images/projects/minesweeper.png",
+      imgDescription: "A screenshot of the classic minesweeper. My version doesn't look as good!",
+      link: "https://jordy1311.github.io/minesweeper/"
+    }
+  ],
+  langsData: [
+    {
+      name: "Javascript",
+      img: "/images/langs/javascript-original.svg",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    },
+    {
+      name: "React",
+      img: "/images/langs/react-original.svg",
+      link: "https://reactjs.org/"
+    },
+    {
+      name: "Redux",
+      img: "/images/langs/redux-original.svg",
+      link: "https://redux.js.org/"
+    },
+    {
+      name: "HTML",
+      img: "/images/langs/html5-original-wordmark.svg",
+      link: "https://developer.mozilla.org/en-US/docs/Web/HTML"
+    },
+    {
+      name: "CSS",
+      img: "/images/langs/css3-original-wordmark.svg",
+      link: "https://developer.mozilla.org/en-US/docs/Web/CSS"
+    },
+    {
+      name: "Express",
+      img: "/images/langs/express-original.svg",
+      link: "https://expressjs.com/"
+    },
+    {
+      name: "Handlebars",
+      img: "/images/langs/handlebars-original.svg",
+      link: "https://handlebarsjs.com/"
+    }
+  ]
+}
