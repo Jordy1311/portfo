962d9e34df5698079b0a04c179e0dc65d3a4f8b0
Jordan Cooper <jordancooper1195@gmail.com>
2021-10-05 17:52:58 +1300

    small link classes that were missing

--- client/components/ContactMe.jsx
+++ client/components/ContactMe.jsx
@@ -8,7 +8,7 @@ const ContactMe = () => {
       <div id='ContactMe'>
         <h1>contact me</h1>
         <h2>Whoever, wherever, whenever,<br></br>I'd love to hear from you!</h2>
-        <a href="#">email me!</a>
+        <a href="#" className='emailme--link'>email me!</a>
         <div className="socials">
           <img src="" alt="" />
         </div>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
         <img src='/images/logo-color.png' alt="My cool logo is <j.cooper> with colored in o's" className='nav--logo' />
       </a>
       <div className='nav--linkgroup'>
-        <a href="#Landing">home</a>
+        <a href="#Landing" className='nav--link'>home</a>
         <a href='#AboutMe' className='nav--link'>about</a>
         <a href='#Projects' className='nav--link'>projects</a>
         <a href='#ContactMe' className='nav--link'>contact</a>
