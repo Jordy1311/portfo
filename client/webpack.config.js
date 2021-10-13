b2969f27a824bc250e541e955ce64428d5004277
Jordan Cooper <jordancooper1195@gmail.com>
2021-10-13 13:48:30 +1300

    convert css >> scss

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,45 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, 'index.js'),
+  entry: [path.join(__dirname, 'index.js'), path.join(__dirname, 'styles/index.scss')],
   output: {
     path: path.join(__dirname, '../server/public'),
     filename: 'bundle.js'
   },
   mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false
+    })
+  ],
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
