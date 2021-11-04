410a36753797f17112c61a5d0188da2ed2eac5ed
Jordan Cooper <jordancooper1195@gmail.com>
2021-11-05 11:54:34 +1300

    making video play onMouseOver and pause onMouseOut

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Project = ({ project }) => {
   return (
     <a href={project.link} target='__blank' className='Project'>
-      <video autoPlay loop muted playsInline className='project--image'>
+
+      <video 
+        loop
+        muted
+        playsInline
+        onMouseOver={e => e.target.play()}
+        onMouseOut={e => e.target.pause()}
+        className='project--image'>
           <source src={project.img} type="video/webm" />
       </video>
 
