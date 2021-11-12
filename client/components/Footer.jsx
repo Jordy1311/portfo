ddef346ac728ef23f8e1a5798f77950facd1b7ef
Jordan Cooper <jordancooper1195@gmail.com>
2021-11-12 14:16:15 +1300

    add link and more to footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='Footer'>
-      <p>Handmade with ❤️ by Jordan.</p>
+      <p>Handmade with <a href='https://placedog.net/1280/720?random' target='__blank'>❤️</a> and ☕️ by Jordan.</p>
     </div>
   )
 }
