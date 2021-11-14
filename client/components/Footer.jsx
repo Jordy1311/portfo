5f5fe12e4766dc6270553a072664c351cc230368
Jordan Cooper <jordancooper1195@gmail.com>
2021-11-14 16:06:11 +1300

    added coffee link to footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='Footer'>
-      <p>Handmade with <a href='https://placedog.net/1280/720?random' target='__blank'>❤️</a> and ☕️ by Jordan.</p>
+      <p>Handmade with
+        <a href='https://placedog.net/1280/720?random' target='__blank'> ❤️ </a> and
+        <a href='https://peoplescoffee.co.nz/products/ethiopia-guji' target='__blank'> ☕️ </a>
+        by Jordan.</p>
     </div>
   )
 }
