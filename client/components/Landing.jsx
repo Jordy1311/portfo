6f4a49872e79f3a532e788d56e610008ab4c36d9
Jordan Cooper <jordancooper1195@gmail.com>
2021-11-22 13:22:09 +1300

    change heading and page title

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -12,11 +12,11 @@ const Landing = () => {
         <div className='landing--group'>
           <img src='/images/me-cartoon-sitting.svg' alt="Cartoon me sitting on the ground" className='landing--group--photo' />
           <div className="landing--group--text">
-            <h1>Hi, I'm Jordan 👋</h1>
+            <h1>👋 Hi, I'm Jordan</h1>
 
             <LandingTypewriter />
 
-            <h2>Full-stack Software Developer keen to make waves not splashes. </h2>
+            <h2>Full-stack Software Developer making waves not splashes.</h2>
           </div>
         </div>
       </div>
